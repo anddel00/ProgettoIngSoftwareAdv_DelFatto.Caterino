78808b05dbc5f3dba881383a49c82ff48130d1f9
Andrea Del Fatto <andrea.delfatto@edu.unife.it>
2026-04-17 12:47:47 +0200

    feat: aggiornata configurazione API e Dockerfile per gestione ambiente
    - Frontend: sostituita `baseURL` in `api.js` con variabile `VITE_API_URL` per una gestione dinamica degli endpoint.
    - Configurazione: aggiunti file `.env.development` e `.env.production` per definire gli URL delle API in ambienti diversi.
    - Dockerfile: configurazione personalizzata di Nginx per supportare il routing di Vue.js.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Creiamo un'istanza personalizzata di Axios
 const api = axios.create({
     // import.meta.env è il modo in cui Vite legge il file .env
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json'
     }
